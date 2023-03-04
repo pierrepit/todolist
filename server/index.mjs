e3ed780e89048acfc78b28e4c22b903327ceaeeb
Pierre <pierrepitoiset77@gmail.com>
2023-03-05 00:23:10 +0100

    tried without app.use

--- server/index.mjs
+++ server/index.mjs
@@ -14,12 +14,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 await db.connect();
-app.use(cors());
+/* app.use(cors());
 //app.use(express.urlencoded({ extended: true })); //useful for POST/PUT requests. Recognize incoming req as a String/Array. If extended, you can post nested objects.
 app.use(express.json()); //useful for POST/PUT requests. Recognize incoming req as a JSON
 app.use('/items', api.router); //router before static files
 app.use(express.static(path.join(__dirname, '/front/build'))); //public = the front file where all the statics (including index.html) are
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/front/build/index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/front/build/index.html'))); */
 
 //starts the Express server
 const server = app.listen(PORT, HOST, () => {
