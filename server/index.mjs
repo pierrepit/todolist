684f24b4cda4f78e2579a6f5d8a3c5e3a7bc6d89
Pierre <pierrepitoiset77@gmail.com>
2023-03-05 00:27:54 +0100

    with app.use json

--- server/index.mjs
+++ server/index.mjs
@@ -15,9 +15,9 @@ const __dirname = path.dirname(__filename);
 
 await db.connect();
 app.use(cors());
-/*//app.use(express.urlencoded({ extended: true })); //useful for POST/PUT requests. Recognize incoming req as a String/Array. If extended, you can post nested objects.
+//app.use(express.urlencoded({ extended: true })); //useful for POST/PUT requests. Recognize incoming req as a String/Array. If extended, you can post nested objects.
 app.use(express.json()); //useful for POST/PUT requests. Recognize incoming req as a JSON
-app.use('/items', api.router); //router before static files
+/*app.use('/items', api.router); //router before static files
 app.use(express.static(path.join(__dirname, '/front/build'))); //public = the front file where all the statics (including index.html) are
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/front/build/index.html'))); */
 
